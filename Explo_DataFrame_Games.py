a60333fe35009e5036cf380ab4a8bd606c25e0ea
MANDON Débora <debora.mandon@gmail.com>
2023-03-03 09:37:05 +0100

    Update Explo_DataFrame_Games.py

--- Explo_DataFrame_Games.py
+++ Explo_DataFrame_Games.py
@@ -69,3 +69,161 @@ g.despine(left=True)
 g.set_axis_labels("Genre", "Count")
 g.set_xticklabels(rotation=70)
 plt.title("Répartition des Genres");
+
+
+fig,ax=plt.subplots(4,3,figsize=(45,25))
+
+coordonnees = [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2),(3,0),(3,1),(3,2)]
+
+for i, j in zip(coordonnees, df['Genre'].unique()):
+    ax[i[0], i[1]].plot_date(x=df[df['Genre']==j]['Year'].values,
+                             y=df[df['Genre']==j]['Global_Sales'].values,
+                             xdate=True,
+                             ls='-')
+    ax[i[0], i[1]].set_title(str(j), fontsize=30);
+
+
+# ETUDE DES VARIABLES NUMERIQUES
+
+for i in var_num:
+    df['cat_'+i]=pd.qcut(df[i], q=[0,.25,.5,.75,1.], duplicates='drop')
+
+df['Year']=pd.to_datetime(df['Year'], format='%Y')
+
+sales_per_year=df.groupby('Year', as_index=False).agg({'NA_Sales':sum, 'EU_Sales':sum, 'JP_Sales':sum, 'Other_Sales':sum,'Global_Sales':sum})
+
+
+sns.set(style="whitegrid")
+plt.figure(figsize=(15,6))
+plt.plot_date(x=sales_per_year['Year'].values,
+              y=sales_per_year['Global_Sales'].values,
+              xdate=True,
+              ls='-',
+              label='Global Sales')
+plt.plot_date(x=sales_per_year['Year'].values,
+              y=sales_per_year['NA_Sales'].values,
+              xdate=True,
+              ls='-',
+              label='North America Sales')
+plt.plot_date(x=sales_per_year['Year'].values,
+              y=sales_per_year['EU_Sales'].values,
+              xdate=True,
+              ls='-',
+              label='Europe Sales')
+plt.plot_date(x=sales_per_year['Year'].values,
+              y=sales_per_year['JP_Sales'].values,
+              xdate=True,
+              ls='-',
+              label='Japan Sales')
+plt.plot_date(x=sales_per_year['Year'].values,
+              y=sales_per_year['Other_Sales'].values,
+              xdate=True,
+              ls='-',
+              label='Other Sales')
+plt.legend(loc='best')
+plt.title('Evolution des ventes')
+plt.show();
+
+
+# ANALYSE DE LA TENDANCE DES VENTES GLOBALES
+
+# Calcul des ventes totales
+total_sales = df.groupby('Year')['Global_Sales'].sum()
+
+# Calcul des ventes totales cumulées
+cumulative_total_sales = total_sales.cumsum()
+
+# Affichage de la série temporelle
+plt.plot_date(cumulative_total_sales.index, cumulative_total_sales.values);
+
+
+plt.figure(figsize = (17,8))
+
+plt.subplot(321)
+sns.boxplot(x = 'NA_Sales', data = df);
+plt.title('Boxplot des Ventes en Amérique du Nord')
+
+plt.subplot(322)
+sns.boxplot(x = 'EU_Sales', data = df)
+plt.title('Boxplot des Ventes en Europe');
+
+plt.subplot(323)
+sns.boxplot(x = 'JP_Sales', data = df);
+plt.title('Boxplot des Ventes au Japon')
+
+plt.subplot(324)
+sns.boxplot(x = 'Other_Sales', data = df)
+plt.title('Boxplot des Ventes dans les Autres Pays');
+
+plt.subplot(325)
+sns.boxplot(x = 'Global_Sales', data = df);
+plt.title('Boxplot des Ventes au Global');
+
+
+# ANALYSE DES VARIABLES QUALITATIVES
+
+Genre_count=pd.DataFrame(df['Genre'].value_counts())
+Platform_count=pd.DataFrame(df['Platform'].value_counts())
+
+plt.figure(figsize = (10,10))
+
+plt.subplot(221)
+sns.boxplot(y = 'Genre', data = Genre_count);
+plt.title('Boxplot des Genres de Jeux Videos')
+
+plt.subplot(222)
+sns.boxplot(y = 'Platform', data = Platform_count)
+plt.title('Boxplot des Platforms de Jeux Videos');
+
+
+# TESTS DE CORRELATION ENTRE LE GLOBAL ET NA_SALES
+
+from scipy.stats import pearsonr
+
+pd.DataFrame(pearsonr(sales_per_year['Global_Sales'], 
+                      sales_per_year['NA_Sales']), 
+                      index = ['pearson_coeff', 'p-value'], 
+                      columns = ['resultat_test'])
+
+# TESTS DE CORRELATION ENTRE LE GLOBAL ET EU_SALES
+
+from scipy.stats import pearsonr
+
+pd.DataFrame(pearsonr(sales_per_year['Global_Sales'], 
+                      sales_per_year['EU_Sales']), 
+                      index = ['pearson_coeff', 'p-value'], 
+                      columns = ['resultat_test'])
+
+# TESTS DE CORRELATION ENTRE LE GLOBAL ET JP_SALES
+
+from scipy.stats import pearsonr
+
+pd.DataFrame(pearsonr(sales_per_year['Global_Sales'], 
+                      sales_per_year['JP_Sales']), 
+                      index = ['pearson_coeff', 'p-value'], 
+                      columns = ['resultat_test'])
+
+# TESTS DE CORRELATION ENTRE LE GLOBAL ET OTHER_SALES
+
+from scipy.stats import pearsonr
+
+pd.DataFrame(pearsonr(sales_per_year['Global_Sales'], 
+                      sales_per_year['Other_Sales']), 
+                      index = ['pearson_coeff', 'p-value'], 
+                      columns = ['resultat_test'])
+
+
+sns.jointplot('NA_Sales', 'Global_Sales', 
+              data = sales_per_year, kind = 'reg');
+
+sns.jointplot('EU_Sales', 'Global_Sales', 
+              data = sales_per_year, kind = 'reg');
+
+sns.jointplot('JP_Sales', 'Global_Sales', 
+              data = sales_per_year, kind = 'reg');
+
+sns.jointplot('Other_Sales', 'Global_Sales', 
+              data = sales_per_year, kind = 'reg');
+
+
+
