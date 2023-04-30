963b466f7906945c2cb24ce0c44ca608dd37e045
MANDON Débora <debora.mandon@gmail.com>
2023-04-30 20:54:58 +0200

    Update tpot_vgsales_ohe_pipeline.py

--- Modele_ML/tpot_vgsales_ohe_pipeline.py
+++ Modele_ML/tpot_vgsales_ohe_pipeline.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
+from sklearn.feature_selection import SelectFwe, f_regression
 from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler
+from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,11 +13,14 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: -1.2420274629143262e-15
-exported_pipeline = LassoLarsCV(normalize=True)
-# Fix random state in exported estimator
-if hasattr(exported_pipeline, 'random_state'):
-    setattr(exported_pipeline, 'random_state', 42)
+# Average CV score on the training set was: -0.0035511951818526072
+exported_pipeline = make_pipeline(
+    MinMaxScaler(),
+    SelectFwe(score_func=f_regression, alpha=0.009000000000000001),
+    LassoLarsCV(normalize=True)
+)
+# Fix random state for all the steps in exported pipeline
+set_param_recursive(exported_pipeline.steps, 'random_state', 42)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
